795139f45b234102310fcbf8c8a0d12f72ca3a4e
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-01-26 01:52:39 +0200

    update

---
+++ 01. JS-Syntax-Fundamentals/P05-Math-operations/index.js
@@ -0,0 +1,28 @@
+function performOperation(num1, num2, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    case "%":
+      result = num1 % num2;
+      break;
+    case "**":
+      result = Math.pow(num1, num2);
+      break;
+    default:
+      console.log("Invalid operator.");
+      return;
+  }
+  console.log(result);
+}
