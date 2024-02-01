228d0c8d058cd5458585bfeace6d1ce94221ca8c
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-02-01 02:24:09 +0200

    update

---
+++ 07. Functions-and-Statements/P05-Simple-calculator/index.js
@@ -0,0 +1,13 @@
+function simpleCalculator(num1, num2, operator) {
+  const operations = {
+    multiply: () => num1 * num2,
+    divide: () => num1 / num2,
+    add: () => num1 + num2,
+    subtract: () => num1 - num2,
+  };
+
+  if (operator in operations) {
+    const result = operations[operator]();
+    console.log(result);
+  }
+}
