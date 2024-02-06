749b04c0d79a1d5a7f67c9e8f67d930b0f71f91b
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-02-07 01:48:58 +0200

    update

---
+++ 13. DOM-Introduction/P04-Show-more/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+  let element = document.querySelector("#text");
+  element.style.display = "inline";
+  let infoText = document.querySelector("#more");
+  infoText.style.display = "none";
+}
